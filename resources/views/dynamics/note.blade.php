cd4f53143f63b976fdfc9e3e59c474f195c9d779
hojinlee <infohojin@gmail.com>
2024-08-13 17:34:05 +0900

    note

---
+++ resources/views/dynamics/note.blade.php
@@ -0,0 +1,6 @@
+<div class="border-start border-4 border-warning rounded-end-4 shadow overflow-hidden py-3 py-sm-4 px-4 px-sm-5">
+    <span class="position-absolute top-0 start-0 w-100 h-100 bg-body-tertiary d-none d-block-dark"></span>
+    <div class="position-relative z-1 py-3">
+        {{$slot}}
+      </div>
+</div>
