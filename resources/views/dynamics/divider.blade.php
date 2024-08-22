9bda0bb6a84b8a061a2a7123c5b2b44f9c0f8de7
hojinlee <infohojin@gmail.com>
2024-08-23 02:05:14 +0900

    divider div width 수정

--- resources/views/dynamics/divider.blade.php
+++ resources/views/dynamics/divider.blade.php
@@ -1,7 +1,12 @@
 <div class="d-flex align-items-center my-4">
-    <hr class="w-100 m-0">
-    <div>
+    {{-- <hr class="w-100 m-0">
+    <div class="">
         {{$slot}}
     </div>
-    <hr class="w-100 m-0">
+    <hr class="w-100 m-0"> --}}
+    <hr class="flex-grow-1 m-0">
+    <div class="px-3 flex-shrink-0">
+        {{$slot}}
+    </div>
+    <hr class="flex-grow-1 m-0">
 </div>
